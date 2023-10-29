c6760d1659c8693c8f0c6632b63c1d7083252bb3
Amanda Brescia <amandasantisouza@gmail.com>
2023-10-28 22:07:22 -0300

    Adicionar arquivos de projeto.

---
+++ dev-back-end-2023/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dev_back_end_2023.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
